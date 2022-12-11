8ef36654b53c973c29ead7f50cfabce313519771
sathoune <michal5bargiel@gmail.com>
2022-12-11 20:55:10 +0100

    ⚗️ Example notes.

--- components/Slides/00-Intro.tsx
+++ components/Slides/00-Intro.tsx
@@ -4,6 +4,11 @@ const Title = () => {
   return (
     <section>
       <h2>{titleText}</h2>
+      <aside className={"notes"}>
+        <p>{"This is a sample speaker's note"}</p>
+        <p>{"This is a sample speaker's note"}</p>
+        <p>{"This is a sample speaker's note"}</p>
+      </aside>
     </section>
   );
 };
